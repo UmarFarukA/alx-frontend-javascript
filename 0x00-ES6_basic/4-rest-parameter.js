bba8d60f40c79e1bb7343bc81a77aa2df6151b9b
Umar Faruk Abdulhamid <talk2ufaz@gmail.com>
2023-06-28 16:45:54 +0100

    remove

--- 0x00-ES6_basic/4-rest-parameter.js
+++
@@ -1,8 +0,0 @@
-export default function returnHowManyArguments(...x) {
-    let count = 0;
-    for (let index = 0; index < x.length; index++) {
-        count += index;
-    }
-
-    return count;
-}
--- 0x00-ES6_basic/README.md
+++
@@ -1,1 +0,0 @@
-The first project of ES6
