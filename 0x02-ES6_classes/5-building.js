996a91cc6218f4321196f86fcbca96c560b07436
Umar Faruk Abdulhamid <talk2ufaz@gmail.com>
2023-06-29 17:47:04 +0100

    remove:set method

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,14 @@
 /* eslint-disable */
-
 export default class Building {
     constructor(sqft) {
-        this.sqft = sqft;
+        this._sqft = sqft;
     }
 
     get sqft() {
         return this._sqft;
     }
 
-    set sqft(val) {
-        this._sqft = val;
-    }
+
 
     evacuationWarningMessage() {
         throw new Error(`Class extending Building must 
