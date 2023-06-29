f7c0a1dfa5cec06d79328a18debac2c049f60f06
Umar Faruk Abdulhamid <talk2ufaz@gmail.com>
2023-06-29 18:39:22 +0100

    fix:indention

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -2,7 +2,10 @@
 function getFullResponseFromAPI(success) {
     return new Promise((resolve, reject) => {
         if (success) {
-            resolve({ status: 200, body: 'Success' });
+            resolve({
+                status: 200,
+                body: 'Success'
+            });
         } else {
             reject(new Error('The fake API is not working currently'))
         }
