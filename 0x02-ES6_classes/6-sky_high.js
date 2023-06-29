134bd6317c8ab325edd46ef94b382da505bed32e
Umar Faruk Abdulhamid <talk2ufaz@gmail.com>
2023-06-29 22:12:02 +0100

    add:constructor def

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,8 +3,8 @@
 import Building from "./5-building";
 
 export default class SkyHighBuilding extends Building {
-    constructor(floors) {
-        super();
+    constructor(sqft, floors) {
+        super(sqft);
         this._floors = floors;
     }
 
