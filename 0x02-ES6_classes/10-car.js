4c245b0b6eb9a3a7e1dc5e1edc02e4c4f9137e75
Umar Faruk Abdulhamid <talk2ufaz@gmail.com>
2023-06-29 21:38:30 +0100

    initial commit

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+
+export default class Car {
+
+    constructor(brand = String, motor = String, color = String) {
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+    }
+
+    cloneCar() {
+        return new Car();
+    }
+}
