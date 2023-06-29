e056a4310009ebb36e33b3cc12c90b86304b8656
Umar Faruk Abdulhamid <talk2ufaz@gmail.com>
2023-06-29 22:45:29 +0100

    add:return

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,8 +1,4 @@
+/* eslint-disable */
 export default function returnHowManyArguments(...x) {
-    let count = 0;
-    for (let index = 0; index < x.length; index++) {
-        count += index;
-    }
-
-    return count;
+    return x.length;
 }
