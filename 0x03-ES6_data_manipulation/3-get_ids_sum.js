c89c401fd2c91b9b6d611d65ab27b2ba695f55b4
Umar Faruk Abdulhamid <talk2ufaz@gmail.com>
2023-07-04 09:42:44 +0100

    correct indentation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,7 @@
 /* eslint-disable */
 export default function getStudentIdsSum(students) {
-    const arr = students.reduce((result, item) => result + item.id, 0);
+    const arr = students.reduce(
+        (result, item) => result + item.id, 0
+    );
     return arr;
 }
